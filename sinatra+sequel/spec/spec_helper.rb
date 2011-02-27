0ced898192907550fbc809a341ce8b8bc46672cd
Wu Jiang (nouse) <masterwujiang@gmail.com>
2011-02-27 23:11:40 +0800

    removed dependency of spork

--- sinatra+sequel/spec/spec_helper.rb
+++ sinatra+sequel/spec/spec_helper.rb
@@ -1,4 +1,3 @@
-require 'spork'
 require 'steak'
 require 'capybara/rspec'
 require 'faker'
@@ -6,28 +5,22 @@ require 'sinatra'
 require 'sequel'
 require 'spawn'
 
-Spork.prefork do
+SINATRA_ROOT = File.dirname(__FILE__)+'/..' 
+$LOAD_PATH << SINATRA_ROOT
+DB = Sequel.amalgalite
+set :environment, :test
+set :root, SINATRA_ROOT
+Capybara.app = Sinatra::Application
+require 'notes'
 
-  SINATRA_ROOT = File.dirname(__FILE__)+'/..' 
-  $LOAD_PATH << SINATRA_ROOT
-  DB = Sequel.amalgalite
-  set :environment, :test
-  set :root, SINATRA_ROOT
-  Capybara.app = Sinatra::Application
+RSpec.configure do |config|
+  config.include TextHelper
 end
 
-Spork.each_run do
-  require 'notes'
-
-  RSpec.configure do |config|
-    config.include TextHelper
-  end
-
-  Note.extend Spawn
-  Note.spawner do |user|
-    user.title = generate_description
-    user.body  = generate_content
-  end
+Note.extend Spawn
+Note.spawner do |user|
+  user.title = generate_description
+  user.body  = generate_content
 end
 
 def generate_description
